855e16f1b0516f7d4aa71cff96c947687ba0b362
tienshaoku <tienshaoku@gmail.com>
2020-07-09 15:45:43 +0800

    # Sidebar 90% completed.

--- src/pages/navigation.js
+++ src/pages/navigation.js
@@ -4,150 +4,182 @@ import logo from '../assets/imslab_logo.jpg';
 import menu_icon from '../assets/menu_icon.png';
 import './navigation.css';
 
-const Navigation = () => {
-    /*
-    return (
-        <div className="dt w-100">
-            <div class="dt w-100 mb3 pt2">
+class Navigation extends React.Component {
+    openNav() {
+        document.getElementById("sideNavbar").style.width = "250px";
+    }
+
+    closeNav() {
+        document.getElementById("sideNavbar").style.width = "0";
+    }
+
+    render() {
+        return (
+            <nav className="dt w-100 pb4 response960">
                 <div
-                    className="dtc-l ph2 tl disappear1145"
-                    style={{ width: "35%" }}
+                    id="sideNavbar"
+                    className="h-100 fixed bg-dark-gray pt5"
+                    style={{
+                        width: "0",
+                        zIndex: "10",
+                        top: "0",
+                        left: "0",
+                        overflowX: "hidden",
+                        transition: "0.5s"
+                    }}
                 >
-                    <a className="dark-gray db f5 f5-l fw5 ph3 pb2">智慧化行動服務實驗室 @ 國立成功大學</a>
-                    <a className="dark-gray db f5 f5-l fw5 ph3">Intelligent Mobile Service Laboratory @ NCKU</a>
-                    <p></p>
-                </div>
-                <div class="dn ph2 tr">
-                    X
-                </div>
-
-                <div class="dtc-l ph2 tr disappear1145">
+                    <p
+                        className="link dim near-white absolute db pv3 ph4 fw6 f3 pv0 mv0 pointer"
+                        style={{
+                            top: "0",
+                            right: "0px"
+                        }}
+                        onClick={() => this.closeNav()}
+                    >←</p>
+                    <p className="link near-white db pt2 f5 f5-l tl b-white mh4 mb0 mt2">智慧化行動服務實驗室</p>
+                    <p className="link near-white db f5 f5-l tl b-white mh4 mt0">@ 國立成功大學</p>
+                    <p className="link near-white db pb2 f5 f5-l tl bb b-near-white mh4 mb4 mt0">Intelligent Mobile Service Laboratory @ NCKU</p>
                     <Link
-                        className="mt2 link dim grow dark-gray f5 f5-l dib ph3 pb2"
+                        className="link dim near-white db pl4 pv2 f5 f5-l tl fw5"
+                        style={{
+                            transition: "0.3s",
+                            letterSpacing:  "0.05em"
+                        }}
                         to="/"
-                    >首頁 Home
-                        </Link>
+                        onClick={() => this.closeNav()}
+                    >首頁 Home</Link>
                     <Link
-                        className="link dim grow dark-gray f5 f5-l dib ph3 pb2"
+                        className="link dim near-white db pl4 pv2 f5 f5-l tl fw5"
+                        style={{
+                            transition: "0.3s",
+                            letterSpacing:  "0.05em"
+                        }}
                         to="/researches"
-                    >研究 Researches
-                        </Link>
+                        onClick={() => this.closeNav()}
+                    >研究 Researches</Link>
                     <Link
-                        className="link dim grow dark-gray f5 f5-l dib ph3 pb2"
-                        to="/professor"
+                        className="link dim near-white db pl4 pv2 f5 f5-l tl fw5"
+                        style={{
+                            transition: "0.3s",
+                            letterSpacing:  "0.05em"
+                        }}
                     >成員 Members
-                        </Link>
+                    <small><b> ▽</b></small>
+                    </Link>
                     <Link
-                        className="link dim grow dark-gray f5 f5-l dib ph3 pb2"
+                        className="link dim near-white db pl4 pv2 f5 f5-l tl fw5"
+                        style={{
+                            transition: "0.3s",
+                            letterSpacing:  "0.05em"
+                        }}
                         to="/honors"
-                    >榮譽 Honors
-                        </Link>
+                        onClick={() => this.closeNav()}
+                    >榮譽 Honors</Link>
                     <Link
-                        className="link dim grow dark-gray f5 f5-l dib ph3 pb2"
+                        className="link dim near-white db pl4 pv2 f5 f5-l tl fw5"
+                        style={{
+                            transition: "0.3s",
+                            letterSpacing:  "0.05em"
+                        }}
                         to="/resources"
-                    >相關資源 Resources
-                        </Link>
+                        onClick={() => this.closeNav()}
+                    >相關資源 Resources</Link>
                     <Link
-                        className="link dim grow dark-gray f5 f5-l dib ph3 pb2"
+                        className="link dim near-white db pl4 pv2 f5 f5-l tl fw5"
+                        style={{
+                            transition: "0.3s",
+                            letterSpacing:  "0.05em"
+                        }}
                         to="/contact"
-                    >聯絡我們 Contact
-                        </Link>
+                        onClick={() => this.closeNav()}
+                    >聯絡我們 Contact</Link>
                 </div>
-                
 
-            </div>
-            <img src={logo}
-                className="db w5 ph3 center"
-                style={{ "min-width": "250px" }}
-                alt=""
-            ></img>
+                <img
+                    src={logo}
+                    className="dib dtc-l w4 ph3 pb2"
+                    style={{ "minWidth": "200px" }}
+                    alt=""
+                ></img>
 
-        </div>
-    );
-    */
-
-    return (
-        <nav className="dt w-100 pb4 response960">
-            <img
-                src={logo}
-                className="dib dtc-l w5 ph3 pb2"
-                style={{ "minWidth": "255px" }}
-                alt=""
-            ></img>
-            <div className="mw8 dib dtc-l v-mid w-100 w-75-l tr-l ph3">
-                <div className="ph2 justify-center disappear960">
-                    <a className="dark-gray db f5 f5-l fw5 ph3 mb2">智慧化行動服務實驗室 @ 國立成功大學</a>
-                    <a className="dark-gray db f5 f5-l fw5 ph3">Intelligent Mobile Service Laboratory @ NCKU</a>
-                    <p></p>
-                </div>
-                <button
-                    className="dim dn appear960 mb3 mt1 center pointer br2 shadow-2 pulse">
-                    <img
-                        className="pa2"
-                        src={menu_icon}
-                    />
-                </button>
-
-                <div className="ph2 disappear960">
-                    <Link
-                        className="mt2 link dim grow dark-gray f5 f5-l dib ph3 pb3"
-                        to="/"
-                    >首頁 Home
-                        </Link>
-                    <Link
-                        className="link dim grow dark-gray f5 f5-l dib ph3 pb3"
-                        to="/researches"
-                    >研究 Researches
-                        </Link>
-                    <div className="dib relative dropdown">
-                        <a
-                            className="link dark-gray f5 f5-l dib ph3 pb3"
-                            style={{ cursor: "default" }}
-                        >成員 Members
-                            </a>
-                        <div
-                            className="dn absolute dropdown-content tc ba br1 bg-white"
-                            style={{ "zIndex": "1" }}
-                        >
-                            <Link
-                                className="dim link dark-gray pa2 db tc bb"
-                                to="/professor"
-                            >教授 Professor
-                                </Link>
-                            <Link
-                                className="dim link dark-gray pa2 db tc bb"
-                                to="/students"
-                            >學生 Students
-                                </Link>
-                            <Link
-                                className="dim link dark-gray pa2 db tc"
-                                to="/alumni"
-                            >學成下山 Alumni
-                                </Link>
-                        </div>
+                <div className="mw8 dib dtc-l v-mid w-100 w-75-l tr-l ph3">
+                    <div className="ph2 justify-center disappear960 mr3">
+                        <a className="dark-gray db f5 f5-l fw5 mb2">智慧化行動服務實驗室 @ 國立成功大學</a>
+                        <a className="dark-gray db f5 f5-l fw5 pb2">Intelligent Mobile Service Laboratory @ NCKU</a>
+                        <p></p>
                     </div>
-                    <Link
-                        className="link dim grow dark-gray f5 f5-l dib ph3 pb3"
-                        to="/honors"
-                    >榮譽 Honors
+
+                    <p
+                        className="dim dn appear960 mb3 center pointer mt1 mw3 mw3-ns pulse"
+                        onClick={() => this.openNav()}
+                    >
+                        <img
+                            className="pa2"
+                            src={menu_icon}
+                        />
+                    </p>
+
+                    <div className="pr2 disappear960">
+                        <Link
+                            className="link dim grow dark-gray f5 f5-l dib ph3 pb2"
+                            to="/"
+                        >首頁 Home
                         </Link>
-                    <Link
-                        className="link dim grow dark-gray f5 f5-l dib ph3 pb3"
-                        to="/resources"
-                    >相關資源 Resources
+                        <Link
+                            className="link dim grow dark-gray f5 f5-l dib ph3 pb2"
+                            to="/researches"
+                        >研究 Researches
                         </Link>
-                    <Link
-                        className="link dim grow dark-gray f5 f5-l dib ph3 pb2"
-                        to="/contact"
-                    >聯絡我們 Contact
+                        <div className="dib relative dropdown">
+                            <a
+                                className="link dark-gray f5 f5-l dib ph3 pb2"
+                                style={{ cursor: "default" }}
+                            >成員 Members 
+                            <small><b> ▽</b></small>
+                            </a>
+                            <div
+                                className="dn absolute dropdown-content tc ba br1 bg-white"
+                                style={{ zIndex: "1" }}
+                            >
+                                <Link
+                                    className="dim link dark-gray pa2 db tc bb"
+                                    to="/professor"
+                                >教授 Professor
+                                </Link>
+                                <Link
+                                    className="dim link dark-gray pa2 db tc bb"
+                                    to="/students"
+                                >學生 Students
+                                </Link>
+                                <Link
+                                    className="dim link dark-gray pa2 db tc"
+                                    to="/alumni"
+                                >學成下山 Alumni
+                                </Link>
+                            </div>
+                        </div>
+
+                        <Link
+                            className="link dim grow dark-gray f5 f5-l dib ph3 pb2"
+                            to="/honors"
+                        >榮譽 Honors
+                        </Link>
+                        <Link
+                            className="link dim grow dark-gray f5 f5-l dib ph3 pb2"
+                            to="/resources"
+                        >相關資源 Resources
+                        </Link>
+                        <Link
+                            className="link dim grow dark-gray f5 f5-l dib ph3 pb2"
+                            to="/contact"
+                        >聯絡我們 Contact
                         </Link>
 
+                    </div>
                 </div>
-
-            </div>
-        </nav>
-    );
+            </nav>
+        );
+    }
 }
 
-// export default Navigation;
 export default Navigation;
