61cf005efb1e61f45c111b5610e31103efd492c4
tienshaoku <tienshaoku@gmail.com>
2021-06-13 20:43:58 +0800

    # footer.js refactored.

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,25 +3,33 @@ import React from "react"
 const URL = "https://github.com/ncku-imslab/ncku-imslab.github.io/blob/master/package.json"
 
 const Footer = () => {
+    const logoSec = (
+        <div>
+            © <b>IMS Lab</b> 2020
+        </div>
+    )
+    const urlSec = (
+        <a className={urlSecClass} href={URL} target="_blank" rel="noopener" key="more">
+            More
+        </a>
+    )
+    const moreInfoSec = (
+        <div className={moreInfoSecClass}>
+            Powered by
+            <b> React</b>,<b> Tachyons </b>
+            and {urlSec}
+        </div>
+    )
+
     return (
         <footer className={footerClass}>
-            <div>
-                © <b>IMS Lab</b> 2020
-            </div>
-            <div className={moreInfoClass}>
-                Powered by
-                <b> React</b>,<b> Tachyons </b>
-                and{" "}
-                <a className={urlClass} href={URL} target="_blank" rel="noopener" key="more">
-                    More
-                </a>
-            </div>
+            {logoSec} {moreInfoSec}
         </footer>
     )
 }
 
 export default Footer
 
-const footerClass = "mid-gray f6"
-const moreInfoClass = "pt3"
-const urlClass = "b link dim mid-gray"
+const footerClass = "f6 mid-gray"
+const moreInfoSecClass = "pt3"
+const urlSecClass = "mid-gray b link dim"
