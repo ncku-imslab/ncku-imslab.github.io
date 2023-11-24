a4c6285418932ce666ad1030a3beb5d44285989a
menghsun <tsaimh@gmail.com>
2023-11-24 23:45:38 +0000

    add MOST project

--- src/data/researches/researches.js
+++ src/data/researches/researches.js
@@ -1,37 +1,15 @@
 const researchesData = {
     "研究 Research": `
-**本實驗室的研究主軸為：行動網路設計與分析。**
+**本實驗室的研究主軸為：
+(1) 6G 行動網路,
+(2) 軟體定義網路,
+(3) 物聯網,
+(4) 網路安全。**
 
-**Main research fields of the lab include wireless communications, cloud computing, mobile network design and analysis.**
+**Main research fields of the lab include 6G mobile network, software defined networking, Internet of Things (IoT) and cybersecurity.**
     
 ---
     
-### 近期研究主題 Recent Research Fields
-
-* Software Defined Network (SDN) and Network Function Virtualization (NFV) (軟體定義網路/網路功能虛擬化)
-
-* Machine-to-Machine (M2M) Communication / Machine-type Communication (MTC) (機器類型通訊)
-
-* Local IP Access (LIPA)
-
-* Device-to-Device (D2D) Communication
-
-* Cloud Radio Access Network (C-RAN)
-
-* Narrow Band IoT (NBIoT)
-    
-### 近五年之研究主題 Past Research Fields 
-* Enhanced Inter-Cell Interference Coordination (eICIC)
-
-* Positioning Methods in Cellular Network (行動定位技術)
-
-* Emergency Communications (緊急通訊技術)
-
-* System Design for Telecom-grade Information System (電信企業等級資訊系統設計)
-
-* IP Multimedia Subsystem (IMS; IP 多媒體子系統)
-
-* Voice Call Continuity (VCC; 語音通話連續性)
 `,
 }
 
